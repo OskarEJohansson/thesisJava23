40062e9fd23a31aa9fa5808ae9e54931bfbc9b5f
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-11-28 09:11:20 +0100

    updated ci config. Added list files after build to find where the jwt.jar is located

--- jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
+++ jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
@@ -24,7 +24,7 @@ class AuthController(
     private val authenticationManager: AuthenticationManager,
 ) {
 
-
+    // TODO: log
     private val LOG: Logger = LoggerFactory.getLogger(AuthController::class.java)
 
     @PostMapping("/v1/login")
