be3537ac070649ddef5bcbbfb06665e336c43f96
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-11-28 11:28:23 +0100

    ci push

--- jwt/src/main/kotlin/dev/oskarjohansson/api/controller/PublicKeyController.kt
+++ jwt/src/main/kotlin/dev/oskarjohansson/api/controller/PublicKeyController.kt
@@ -18,6 +18,7 @@ class PublicKeyController(val rsaKey: RSAKey) {
 
     private val LOG: Logger = LoggerFactory.getLogger(PublicKeyController::class.java)
 
+    // TODO: 12 
     @GetMapping("/v1/public-key")
     fun publicKey(request: HttpServletRequest): ResponseEntity<PublicKeyResponseDTO> =
         runCatching {
