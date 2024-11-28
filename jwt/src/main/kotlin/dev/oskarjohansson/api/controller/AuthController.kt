cde77de7b1fd8e217c341526ff6cb6b34e464306
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-11-28 10:29:19 +0100

    try xx of ci push to artifact registry

--- jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
+++ jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
@@ -24,7 +24,6 @@ class AuthController(
     private val authenticationManager: AuthenticationManager,
 ) {
 
-    // TODO:  1
     private val LOG: Logger = LoggerFactory.getLogger(AuthController::class.java)
 
     @PostMapping("/v1/login")
