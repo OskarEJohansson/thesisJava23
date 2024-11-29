3af7a5417aceb9e9aa8edf3c83d8c8c57a5c5eee
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-11-29 09:12:34 +0100

    ci push 1235645745673465345

--- jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
+++ jwt/src/main/kotlin/dev/oskarjohansson/api/controller/AuthController.kt
@@ -24,6 +24,7 @@ class AuthController(
     private val authenticationManager: AuthenticationManager,
 ) {
 
+    // TODO: §12 
     private val LOG: Logger = LoggerFactory.getLogger(AuthController::class.java)
 
     @PostMapping("/v1/login")
