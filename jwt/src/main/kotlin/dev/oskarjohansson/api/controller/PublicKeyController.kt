c01368062bfdbe4fcf94ba33042887a6b00f2fe9
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-11-29 11:14:52 +0100

    I have identified the problem with the docker/login-action. The pipeline should work as expected now

--- jwt/src/main/kotlin/dev/oskarjohansson/api/controller/PublicKeyController.kt
+++ jwt/src/main/kotlin/dev/oskarjohansson/api/controller/PublicKeyController.kt
@@ -15,10 +15,8 @@ import java.util.*
 @RequestMapping("/public-key-controller")
 class PublicKeyController(val rsaKey: RSAKey) {
 
-
     private val LOG: Logger = LoggerFactory.getLogger(PublicKeyController::class.java)
 
-    // TODO: 123asd 
     @GetMapping("/v1/public-key")
     fun publicKey(request: HttpServletRequest): ResponseEntity<PublicKeyResponseDTO> =
         runCatching {
