da585f0714dcdd6d88ea88f0274f8a3f87437052
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-12-14 09:10:52 +0100

    added default pass to smtp.pass

--- library/src/main/kotlin/dev/oskarjohansson/configuration/MailServiceConfiguration.kt
+++ library/src/main/kotlin/dev/oskarjohansson/configuration/MailServiceConfiguration.kt
@@ -1,5 +1,6 @@
 package dev.oskarjohansson.configuration
 
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
