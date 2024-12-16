bf94f4384d3b3e5b8ee22e62d1b05b427130abb3
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-12-16 15:44:31 +0100

    test to build admin

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -10,9 +10,6 @@ kotlin {
     compilerOptions { jvmTarget.set(JvmTarget.JVM_21) }
 }
 
-group = "com.OskarJohansson"
-version = "0.0.1-SNAPSHOT"
-
 allprojects{
     repositories {
         mavenCentral()
