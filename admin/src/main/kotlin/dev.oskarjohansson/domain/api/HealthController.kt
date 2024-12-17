68a8232c4e6014b7ab4bf309df67aeb7509b06fd
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-12-17 14:23:52 +0100

    Change in health check controller

--- admin/src/main/kotlin/dev.oskarjohansson/domain/api/HealthController.kt
+++ admin/src/main/kotlin/dev.oskarjohansson/domain/api/HealthController.kt
@@ -1,13 +1,21 @@
 package dev.oskarjohansson.domain.api
 
 
+import dev.oskarjohansson.service.MailService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
 
+    private var LOG: Logger = LoggerFactory.getLogger(MailService::class.java)
 
     @GetMapping("/health")
-    fun healthCheck() = "OK"
+    fun healthCheck(): ResponseEntity.BodyBuilder {
+        LOG.info("Health check complete")
+        return ResponseEntity.ok()
+    }
 }
